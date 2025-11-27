8df0cedaff6ccf830f26024545f6c1fa4a710a15
Animesh <animeshiit0806@gmail.com>
2025-11-28 00:51:03 +0530

    initial commit

---
+++ backend/src/main/java/com/codeastras/backend/codeastras/CodeastrasApplication.java
@@ -0,0 +1,13 @@
+package com.codeastras.backend.codeastras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeastrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeastrasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codeastras/backend/codeastras/CodeastrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeastras.backend.codeastras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeastrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
